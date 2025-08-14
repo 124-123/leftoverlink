f59b47300fb59b2124183c0822ff416af9da9552
124-123 <shivamsandippawar@gmail.com>
2025-08-14 13:11:39 +0530

    Initial commit for Render deployment

--- leftoverlink/wsgi.py
+++ leftoverlink/wsgi.py
@@ -6,12 +6,15 @@ It exposes the WSGI callable as a module-level variable named ``application``.
 For more information on this file, see
 https://docs.djangoproject.com/en/5.2/howto/deployment/wsgi/
 """
-
 import os
-
 from django.core.wsgi import get_wsgi_application
+from whitenoise import WhiteNoise
+from pathlib import Path
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'leftoverlink.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "leftoverlink.settings")
 
 application = get_wsgi_application()
 
+# Ensure STATIC_ROOT is correct
+BASE_DIR = Path(__file__).resolve().parent.parent
+application = WhiteNoise(application, root=BASE_DIR / "staticfiles")
