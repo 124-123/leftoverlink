57458ee6be865fbc043d07da3ecaa39eaa30b8e0
124-123 <shivamsandippawar@gmail.com>
2025-08-14 10:04:40 +0530

    Initial commit

--- leftoverlink/settings.py
+++ leftoverlink/settings.py
@@ -77,13 +77,15 @@ WSGI_APPLICATION = 'leftoverlink.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
+import dj_database_url
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        default='postgresql://leftoverlink_user:yUoSJ61lGvBbn7eOLtxNMt2Tq8wOXMIY@dpg-d2emd0juibrs7385v6g0-a.oregon-postgres.render.com/leftoverlink',
+        conn_max_age=600
+    )
 }
+
 
 
 # Password validation
